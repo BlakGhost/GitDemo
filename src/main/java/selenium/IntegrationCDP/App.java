2d15b1f5d9650dfb06fec493cf96279c0c0d0f47
akshay <akshaysinghal15795@gmail.com>
2022-11-25 18:55:53 +0530

    first commit

---
+++ src/main/java/selenium/IntegrationCDP/App.java
@@ -0,0 +1,13 @@
+package selenium.IntegrationCDP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/selenium/IntegrationCDP/GeoLocation.java
@@ -0,0 +1,10 @@
+package selenium.IntegrationCDP;
+
+public class GeoLocation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
